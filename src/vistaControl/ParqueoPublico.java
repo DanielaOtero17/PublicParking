aa02e50bdeed458259151043e288aac1fa4f1c37
Dilan Daniel Bustamante Martinez <1144209785@academico.icesi.edu.co>
2019-01-25 12:33:23 -0500

    add title to I G

--- src/vistaControl/ParqueoPublico.java
+++ src/vistaControl/ParqueoPublico.java
@@ -12,9 +12,10 @@ public class ParqueoPublico extends Application {
       public void start(Stage stage) throws Exception{
          Parent root =
             FXMLLoader.load(getClass().getResource("ParqueoPublico.fxml"));
+           
 
          Scene scene = new Scene(root); // attach scene graph to scene
-         // To add a line
+         stage.setTitle("Parqueo Público");
          stage.setScene(scene); // attach scene to stage
          stage.show(); // display the stage
       }
